df4931c72b7728d4f32ab77b2031b3d18310638f
Sunil Mallya <mallya16@gmail.com>
2013-06-30 23:29:44 -0700

    update build script

--- api/build.sh
+++ api/build.sh
@@ -2,6 +2,6 @@ python -m py_compile *.py
 if [ $? -eq 0 ]
 	then x=$(git rev-list HEAD | wc -l)
 	echo $x > code.version
-	tar cvzf neon-code.tar.gz --exclude={lib,result,results,*.pyc,build.sh,*.mov,README*} *
+	tar cvzfh neon-code.tar.gz --exclude={lib,result,results,*.pyc,build.sh,,README*,.git} *
 fi
 
