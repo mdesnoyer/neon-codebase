e9bce60de96498f0f441fde1af5498e89930eb93
Mark Desnoyer <desnoyer@neon-lab.com>
2013-07-29 18:03:17 -0700

    New tools in the video id scrapper to search through similar videos

--- expPipeline/youtube_video_id_scraper.py
+++ expPipeline/youtube_video_id_scraper.py
@@ -3,13 +3,20 @@
 '''
 USAGE = '%prog [options]'
 
+import logging
 from optparse import OptionParser
 import xml.etree.ElementTree
+import random
 import re
 import urllib
 import urllib2
 import string
 import sys
+
+from apiclient.discovery import build as api_build
+
+YOUTUBE_KEY = None
+_log = logging.getLogger(__name__)
 
 def generate_url(query, cur_idx):
     return 'http://gdata.youtube.com/feeds/api/videos?' + \
@@ -51,6 +58,93 @@ def generate_video_ids(query, n_videos=25, max_duration=600):
         finally:
             xmlStream.close()
 
+def find_similar_videos(video_id, n_videos=100):
+    '''Returns a generator for similar videos.
+
+    Inputs:
+    video_id: youtube video id to find similar videos to. 
+    n_videos: max number of videos to return
+
+    returns:
+    A generator that spits out youtube video_ids of similar videos.
+    '''
+    yt_service = api_build('youtube', 'v3', developerKey=YOUTUBE_KEY)
+    videos_found = 0
+
+    try:
+        cur_response = yt_service.search().list(
+            part='id',
+            maxResults=min(n_videos, 50),
+            relatedToVideoId=video_id,
+            type='video').execute()
+    except IOError as e:
+        _log.error('Error querying youtube: %s' % e)
+        return
+    while cur_response:
+        for result in cur_response.get("items", [])
+            videos_founds += 1
+            yield result["id"]["videoId"]
+        
+        nextPage = cur_response.get("nextPageToken", None)
+        if nextPage is None:
+            break
+        try:
+            cur_response = yt_service.search().list(
+                part='id',
+                maxResults=min(n_videos - videos_found, 50),
+                relatedToVideoId=video_id,
+                type='video',
+                pageToken=nextPage).execute()
+        except IOError as e:
+           _log.error('Error querying youtube: %s' % e)
+           return
+
+def get_video_duration(video_id):
+    '''Retrieves the duration of the video.'''
+    yt_service = api_build('youtube', 'v3', developerKey=YOUTUBE_KEY)
+
+    try:
+        response = yt_service.videos.list(
+            id=video_id,
+            part='contentDetails')
+    except IOError as e:
+        _log.error('Error getting the video length: %s' % e)
+        return 0
+
+    timeRe = re.compile('PT([0-9]+)M([0-9]+)S')
+    
+    parse = timeRe.search(response["items"][0]["contentDetails"]["duration"])
+    return 60 * int(parse.groups()[1]) + int(parse.groups()[2])
+
+def get_new_videos(video_ids, old_video_ids, max_duration=600,
+                   n_videos=25):
+    '''Retrieves videos similar to video_ids, skipping old ones.
+
+    Inputs:
+    video_ids - List of video ids to find ones similar to it
+    old_video_ids - list of video ids to exclude from returning
+    max_duration - Maximum duration of the video
+    n_videos - Number of videos to return total
+
+    Outputs:
+    list of new video_ids similar to those in video_ids
+    '''
+    retval = []
+    q = []
+    q.update(video_ids)
+    n_found = 0
+    while len(q) > 0 and n_found < n_videos:
+        random.shuffle(q)
+        video_id = q.pop()
+        for candidate in find_similar_videos(video_id):
+            if candidate in old_video_ids:
+                q.append(candidate)
+            else:
+                if get_video_duration(candidate) < max_duration:
+                    retval.append(candidate)
+                    n_found += 1
+
+    return retval
 
 if __name__ == '__main__':
     parser = OptionParser(usage=USAGE)
@@ -63,8 +157,17 @@ if __name__ == '__main__':
                       help='Number of videos to retrieve per query')
     parser.add_option('--max_duration', type='int', default=600,
                       help='Maximum duration in seconds')
+    parser.add_option('--yt_key',
+                      default='AIzaSyCI1sGIS5svU8FO6cd7S4XG-Z9EvN0DYHE',
+                      help='YouTube API key')
+    parser.add_option('--seed', type='int', default=19987,
+                      help='Seed for the random number generator')
 
     options, args = parser.parse_args()
+
+    YOUTUBE_KEY = options.yt_key
+    random.seed(options.seed)
+    logging.basicConfig(level=logging.INFO)
 
     inStream = sys.stdin
     if options.input is not None:
