a00035c9be5872235d6e9358affab475a8ece685
Mark Desnoyer <desnoyer@neon-lab.com>
2013-10-24 16:05:36 -0700

    Options module now automatically polls for updates to the file, both from S3 or from the filesystem.

--- utils/test/options_test.py
+++ utils/test/options_test.py
@@ -14,9 +14,11 @@ import boto
 import boto.exception
 import fake_filesystem
 import mock
+from mock import patch
 import os
 from StringIO import StringIO
 import options_test_module as test_mod
+import time
 import unittest
 import utils.options
 
@@ -137,7 +139,8 @@ class TestCommandLineParsing(unittest.TestCase):
         self.assertEqual(test_mod.get(self.parser, 'an_int'), 10)
 
 
-    def test_finding_config_file(self):
+    @patch('utils.options.os.path.getmtime', return_value=int(time.time()))
+    def test_finding_config_file(self, mtime_mock):
         self.parser.define('a_float', default=6.5, type=float)
 
         config_file = self.filesystem.CreateFile(
@@ -148,12 +151,44 @@ class TestCommandLineParsing(unittest.TestCase):
 
         self.assertEqual(self.parser.a_float, 10.8)
 
+    def test_changing_config_file(self):
+        '''Test when the config file changes. We want to update the options.'''
+        self.parser.define('a_float', default=6.5, type=float)
+        self.parser.define('an_int', default=3, type=int)
+        self.parser.define('a_string', default='cow')
+
+        config_file = self.filesystem.CreateFile(
+            'my_config.yaml', 
+            contents='a_float: 10.8\nan_int: 6')
+
+        with patch('utils.options.os.path.getmtime',
+                   return_value=int(time.time())) :
+            self.parser.parse_options(['--config', 'my_config.yaml',
+                                       '--an_int', '10'])
+        
+        self.assertEqual(self.parser.a_float, 10.8)
+        self.assertEqual(self.parser.an_int, 10)
+        self.assertEqual(self.parser.a_string, 'cow')
+
+        # Simulate a file change
+        config_file.SetContents('a_float: 20.9\nan_int: 1')
+
+        with patch('utils.options.os.path.getmtime',
+                   return_value=int(time.time() + 10)):
+            self.parser._process_new_config_file(path='my_config.yaml')
+
+        # the int shouldn't change because the command line has precendence
+        self.assertEqual(self.parser.a_float, 20.9)
+        self.assertEqual(self.parser.an_int, 10)
+        self.assertEqual(self.parser.a_string, 'cow')
+
 class TestS3ConfigFiles(unittest.TestCase):
 
     def setUp(self):
         self.parser = utils.options.OptionParser()
 
         self.mock_key = mock.MagicMock()
+        self.mock_key.last_modified = '2012-03-13T03:54:07.000Z'
         self.mock_bucket = mock.MagicMock()
         self.mock_connection = mock.MagicMock()
         self.mock_connection.get_bucket = mock.MagicMock(
@@ -178,6 +213,34 @@ class TestS3ConfigFiles(unittest.TestCase):
         self.assertEqual(self.parser.an_int, 8)
         self.mock_connection.get_bucket.assert_called_with('bucket.now')
         self.mock_bucket.get_key.assert_called_with('path/for/me/config.yaml')
+
+    def test_changing_file_on_s3(self):
+        self.parser.define('a_float', default=6.5, type=float)
+        self.parser.define('an_int', default=6, type=int)
+        self.parser.define('a_string', default='cow')
+
+        self.mock_key.open = mock.MagicMock(return_value=FileStringIO(
+            'an_int: 8\na_float: 10.1'))
+
+        self.parser.parse_options([
+            '--config', 's3://bucket.now/path/for/me/config.yaml',
+            '--an_int', '1'])
+
+        self.assertEqual(self.parser.an_int, 1)
+        self.assertEqual(self.parser.a_float, 10.1)
+        self.assertEqual(self.parser.a_string, 'cow')
+
+        # Now simulate an update
+        self.mock_key.open = mock.MagicMock(return_value=FileStringIO(
+            'an_int: 7\na_float: 13.2'))
+        self.mock_key.last_modified = '2012-03-19T03:54:07.000Z'
+
+        self.parser._process_new_config_file(
+            path='s3://bucket.now/path/for/me/config.yaml')
+
+        self.assertEqual(self.parser.an_int, 1)
+        self.assertEqual(self.parser.a_float, 13.2)
+        self.assertEqual(self.parser.a_string, 'cow')
 
     def test_key_not_in_bucket(self):
         self.parser.define('an_int', default=6, type=int)
