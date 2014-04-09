aeee3f3bc6b102d41b618143f8654bcc8b5483e8
Mark Desnoyer <desnoyer@neon-lab.com>
2014-04-09 11:37:24 -0700

    Change opencv seek so that it's more reliable in the case where the backend can't tell you where it is

--- utils/pycvutils.py
+++ utils/pycvutils.py
@@ -8,8 +8,11 @@ Copyright 2014 Neon Labs
 '''
 
 import cv2
+import logging
 import numpy as np
 from . import imageutils
+
+_log = logging.getLogger(__name__)
 
 def resize_and_crop(image, h, w):
     '''Resizes the image and then crops to a new size.
@@ -42,3 +45,58 @@ def to_pil(im):
 def from_pil(im):
     '''Converts an PIL image to an OpenCV BGR format.'''
     return imageutils.PILImageUtils.to_cv(im)
+
+def seek_video(video, frame_no, do_log=True, cur_frame=None):
+    '''Seeks an OpenCV video to a given frame number.
+
+    After calling this function, the next read() will give you that frame.
+
+    This is necessary because the normal way of seeking in OpenCV
+    (setting the CV_CAP_PROP_POS_FRAMES doesn't always work. It might
+    only go to the previous keyframe, or it might not be possible to
+    get the current frame number).
+
+    Inputs:
+    video - An opencv VideoCapture object
+    frame_no - The frame number to seek to
+    do_log - True if logging should happen on errors
+    cur_frame - If you know the frame number that the video should be at, 
+                put it here. It helps to identify error cases.
+
+    Outputs:
+    Returns (sucess, cur_frame)
+    '''
+
+    grab_sucess = True
+    if (cur_frame is not None and cur_frame > 0 and 
+        video.get(cv2.cv.CV_CAP_PROP_POS_FRAMES) == 0):
+        if do_log:
+            _log.warn('Cannot read the current frame location.'
+                      'Resorting to manual advancing')
+
+        while grab_sucess and cur_frame < frame_no:
+            grab_sucess = video.grab()
+            cur_frame += 1
+
+    else:
+        if (cur_frame is None or not (
+                (frame_no - cur_frame) < 4 and (frame_no - cur_frame) >= 0) ):
+            # Seeking to a place in the video that's a ways away, so JUMP
+            video.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, frame_no)
+            
+        cur_frame = video.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
+        while grab_sucess and cur_frame < frame_no:
+            grab_sucess = video.grab()
+            cur_frame = video.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
+            if cur_frame == 0:
+                if do_log:
+                    _log.error('Cannot read the current frame location. '
+                               'You need to set a cur_frame, but we are '
+                               'resorting to manual assuming that we started '
+                               'at 0.')
+                cur_frame = 1
+                while grab_sucess and cur_frame < frame_no:
+                    grab_sucess = video.grab()
+                    cur_frame += 1
+
+    return grab_sucess, cur_frame
