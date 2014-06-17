e6b0899f763cb29ecf9f32ee886d48425fea5586
Mark Desnoyer <desnoyer@neon-lab.com>
2014-06-16 20:34:14 -0700

    Writing the analytics script

--- stats/generate_analytics_report.py
+++ stats/generate_analytics_report.py
@@ -15,6 +15,7 @@ if sys.path[0] <> base_path:
     sys.path.insert(0, base_path)
 
 from datetime import datetime
+import dateutil.parser
 import impala.dbapi
 import impala.error
 import logging
@@ -37,9 +38,9 @@ define("end_time", default=None, type=str,
 _log = logging.getLogger(__name__)
 
 class ThumbnailStats(object):
-    def __init__(loads=None, visible=None, clicks=None, clicks_to_video=None)
+    def __init__(loads=None, visibles=None, clicks=None, clicks_to_video=None)
         self.loads = loads
-        self.visible = visible
+        self.visibles = visibles
         self.clicks = clicks
         self.clicks_to_video = clicks_to_video
 
@@ -47,15 +48,30 @@ def get_time_clause():
     '''Returns the where clause to make sure the results are between the
     start and end times.
     '''
-    retval = ''
+    clauses = []
     if options.start_time is not None:
-        start_time = 
+        start_time = dateutil.parser.parse(options.start_time)
+        clauses.extend([
+            'mnth >= %i' % start_time.month,
+            'yr >= %i' % start_time.year,
+            'serverTime >= %s' % start_time.isoformat()])
+
+    if options.end_time is not None:
+        end_time = dateutil.parser.parse(options.end_time)
+        clauses.extend([
+            'mnth <= %i' % end_time.month,
+            'yr <= %i' % end_time.year,
+            'serverTime <= %s' % end_time.isoformat()])
+
+    return ' and '.join(clauses)
 
 def collect_counts:
     '''Grabs the visible and click counts from the database.
 
     Returns: thumbnail_id -> ThumbnailStats
     '''
+    retval = {}
+    
     conn = impala.dbapi.connect(host=options.stats_host,
                                 port=options.stats_port)
     cursor = conn.cursor()
@@ -64,10 +80,31 @@ def collect_counts:
     cursor.execute(
     """select thumbnail_id, count(imloadclienttime),
     count(imvisclienttime), count(imclickclienttime) from EventSequences 
-    where tai='%s' and mnth >= %i and mnth <= %i
-    """)
+    where tai='%s' and imloadclienttime is not null and %s
+    """ % (options.pub_id, get_time_clause()))
+
+    for row in cursor:
+        retval[row[0]] = ThumbnailStats(loads=row[1], visibles=row[2],
+                                        clicks=row[3])
+
+    _log.info('Getting all the clicks that led to video plays')
+    cursor.execute(
+        """select thumbnail_id, count(imclickclienttime) from EventSequences
+    where tai='%s' and imloadclienttime is not null and 
+    (adplayclienttime is not null or videoplayclienttime is not null) and %s
+    """ % (options.pub_id, get_time_clause()))
+
+    for row in cursor:
+        try:
+            retval[row[0]].clicks_to_video = row[1]
+        except KeyError:
+            pass
+
+    return retval
 
 def main():
+    counts = collect_counts()
+    thumbnail_info = neondata.ThumbnailMetadata.get_many(counts.keys())
 
 if __name__ == "__main__":
     utils.neon.InitNeon()
