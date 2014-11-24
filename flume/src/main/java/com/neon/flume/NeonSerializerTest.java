b05fb463cc4451e8a247741ad1b1ac0a158479c8
pierredumas <pierre@neon-lab.com>
2014-11-24 13:03:53 -0800

    Update NeonSerializerTest.java

--- flume/src/main/java/com/neon/flume/NeonSerializerTest.java
+++ flume/src/main/java/com/neon/flume/NeonSerializerTest.java
@@ -41,7 +41,7 @@ import org.apache.avro.io.DatumReader;
 import org.apache.avro.io.Decoder;
 import org.apache.avro.io.DecoderFactory;
 import org.apache.avro.io.Encoder;
-import org.apache.avro.io.BinaryDecoder;
+import org.apache.avro.io.BinaryEncoder;
 import org.apache.avro.io.EncoderFactory;
 
 import org.apache.flume.Event;
