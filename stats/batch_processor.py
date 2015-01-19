a13d412c7cfa82fe9fc960ba30b0a1e2a66f1d6f
Mark Desnoyer <desnoyer@neon-lab.com>
2015-01-19 15:26:20 -0800

    Fixing stupid errors in hive code

--- stats/batch_processor.py
+++ stats/batch_processor.py
@@ -118,57 +118,55 @@ class ImpalaTableBuilder(threading.Thread):
                     hive.execute(
                         'SET hive.exec.dynamic.partition.mode=nonstrict')
                         
-                    try:
-                        self.status = 'RUNNING'
 
-                        external_table = 'Avro%ss' % self.event
-                        _log.info("Registering external %s table with Hive" % 
-                                  external_table)
-                        hive.execute('DROP TABLE IF EXISTS %s' % 
-                                     external_table)
-                        hive.execute("""
-                        create external table %s
-                        ROW FORMAT SERDE 
-                        'org.apache.hadoop.hive.serde2.avro.AvroSerDe'
-                        STORED AS
-                        INPUTFORMAT  
-                        'org.apache.hadoop.hive.ql.io.avro.AvroContainerInputFormat'
-                        OUTPUTFORMAT 
-                        'org.apache.hadoop.hive.ql.io.avro.AvroContainerOutputFormat'
-                        LOCATION '%s/%s'
-                        TBLPROPERTIES (
-                        'avro.schema.url'='s3://%s/%s.avsc'
-                        )""" % 
-                        (external_table, self.base_input_path, hive_event, 
-                         options.schema_bucket, hive_event))
+                    self.status = 'RUNNING'
 
-                        parq_table = '%ss' % self.event
-                        _log.info("Building parquet table %s" % parq_table)
-                        hive.execute( """
-                        create table if not exists %s
-                        (%s)
-                        partitioned by (tai string, yr int, mnth int)
-                        STORED AS PARQUET
-                        """ % (parq_table, self._generate_table_definition()))
+                    external_table = 'Avro%ss' % self.event
+                    _log.info("Registering external %s table with Hive" % 
+                              external_table)
+                    hive.execute('DROP TABLE IF EXISTS %s' % 
+                                 external_table)
+                    hive.execute("""
+                    create external table %s
+                    ROW FORMAT SERDE 
+                    'org.apache.hadoop.hive.serde2.avro.AvroSerDe'
+                    STORED AS
+                    INPUTFORMAT  
+                    'org.apache.hadoop.hive.ql.io.avro.AvroContainerInputFormat'
+                    OUTPUTFORMAT 
+                    'org.apache.hadoop.hive.ql.io.avro.AvroContainerOutputFormat'
+                    LOCATION '%s/%s'
+                    TBLPROPERTIES (
+                    'avro.schema.url'='s3://%s/%s.avsc'
+                    )""" % 
+                    (external_table, self.base_input_path, hive_event, 
+                     options.schema_bucket, hive_event))
 
-                        # Building parquet tables takes a lot of
-                        # memory, so make sure we give the job enough.
-                        hive.execute("SET mapreduce.reduce.memory.mb=8000")
-                        hive.execute(
-                            "SET mapreduce.reduce.java.opts=-Xmx6400m")
-                        hive.execute("SET mapreduce.map.memory.mb=8000")
-                        hive.execute("SET mapreduce.map.java.opts=-Xmx6400m")
-                        hive.execute("""
-                        insert overwrite table %s
-                        partition(tai, yr, mnth)
-                        select %s, trackerAccountId,
-                        year(cast(serverTime as timestamp)),
-                        month(cast(serverTime as timestamp)) from %s""" %
-                        (parq_table, ','.join(x.name for x in 
-                                              self.avro_schema.fields),
-                         external_table))
-                    finally:
-                        hive.execute('reset')
+                    parq_table = '%ss' % self.event
+                    _log.info("Building parquet table %s" % parq_table)
+                    hive.execute( """
+                    create table if not exists %s
+                    (%s)
+                    partitioned by (tai string, yr int, mnth int)
+                    STORED AS PARQUET
+                    """ % (parq_table, self._generate_table_definition()))
+
+                    # Building parquet tables takes a lot of
+                    # memory, so make sure we give the job enough.
+                    hive.execute("SET mapreduce.reduce.memory.mb=8000")
+                    hive.execute(
+                        "SET mapreduce.reduce.java.opts=-Xmx6400m")
+                    hive.execute("SET mapreduce.map.memory.mb=8000")
+                    hive.execute("SET mapreduce.map.java.opts=-Xmx6400m")
+                    hive.execute("""
+                    insert overwrite table %s
+                    partition(tai, yr, mnth)
+                    select %s, trackerAccountId,
+                    year(cast(serverTime as timestamp)),
+                    month(cast(serverTime as timestamp)) from %s""" %
+                    (parq_table, ','.join(x.name for x in 
+                                          self.avro_schema.fields),
+                     external_table))
 
             _log.info("Refreshing table %s in Impala" % parq_table)
             impala_cursor = impala_conn.cursor()
