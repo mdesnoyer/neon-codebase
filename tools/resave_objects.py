b12cf8ff2452fb85ca6ab90e8c1f27435dd515d0
Mark Desnoyer <desnoyer@neon-lab.com>
2015-08-01 17:27:35 -0700

    Fix bug

--- tools/resave_objects.py
+++ tools/resave_objects.py
@@ -8,7 +8,7 @@ Copyright Neon Labs 2015
 import os
 import os.path
 import sys
-__base_path__ = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+__base_path__ = os.path.abspath(os.path.join(os.path.dirname(__file__),  '..'))
 if sys.path[0] != __base_path__:
     sys.path.insert(0, __base_path__)
 
