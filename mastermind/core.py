ac161f169ff84fdcc286a12814e067a446d22105
wileyw <wang@neon-lab.com>
2015-08-24 09:28:17 -0700

    Add conditions

--- mastermind/core.py
+++ mastermind/core.py
@@ -614,58 +614,70 @@ class Mastermind(object):
 
         valid_bandits = list(valid_bandits)
 
-        # Now determine the serving percentages for each valid bandit
-        # based on a prior of its model score and its measured ctr.
-        bandit_ids = [x.id for x in valid_bandits]
-        conv = dict([(x.id, self._get_prior_conversions(x) +
-                      x.get_conversions())
-                for x in valid_bandits])
-        imp = dict([(x.id, Mastermind.PRIOR_IMPRESSION_SIZE * 
-                             (1 - Mastermind.PRIOR_CTR) + 
-                             x.get_impressions() - conv[x.id])
-                             for x in valid_bandits])
+        # Decide if experiment has already comes to a conclusion
+        is_exp_concluded = false # TODO: should call a function to decide
+        if not is_exp_concluded:
+            # Now determine the serving percentages for each valid bandit
+            # based on a prior of its model score and its measured ctr.
+            bandit_ids = [x.id for x in valid_bandits]
+            conv = dict([(x.id, self._get_prior_conversions(x) +
+                          x.get_conversions())
+                    for x in valid_bandits])
+            imp = dict([(x.id, Mastermind.PRIOR_IMPRESSION_SIZE * 
+                                 (1 - Mastermind.PRIOR_CTR) + 
+                                 x.get_impressions() - conv[x.id])
+                                 for x in valid_bandits])
 
-        # Run the monte carlo series
-        MC_SAMPLES = 1000.
-        mc_series = [spstats.beta.rvs(max(1, conv[x]),
-                                      max(1, imp[x]),
-                                      size=MC_SAMPLES)
-                                      for x in bandit_ids]
-        if non_exp_thumb is not None:
-            conv = self._get_prior_conversions(non_exp_thumb) + \
-              non_exp_thumb.get_conversions()
-            mc_series.append(
-                spstats.beta.rvs(max(1, conv),
-                                 max(1, Mastermind.PRIOR_IMPRESSION_SIZE * 
-                                        (1 - Mastermind.PRIOR_CTR) + 
-                                        non_exp_thumb.get_impressions()-conv),
-                                 size=MC_SAMPLES))
+            # Run the monte carlo series
+            MC_SAMPLES = 1000.
+            mc_series = [spstats.beta.rvs(max(1, conv[x]),
+                                          max(1, imp[x]),
+                                          size=MC_SAMPLES)
+                                          for x in bandit_ids]
+            if non_exp_thumb is not None:
+                conv = self._get_prior_conversions(non_exp_thumb) + \
+                  non_exp_thumb.get_conversions()
+                mc_series.append(
+                    spstats.beta.rvs(max(1, conv),
+                                     max(1, Mastermind.PRIOR_IMPRESSION_SIZE * 
+                                            (1 - Mastermind.PRIOR_CTR) + 
+                                            non_exp_thumb.get_impressions()-conv),
+                                     size=MC_SAMPLES))
 
-        win_frac = np.array(np.bincount(np.argmax(mc_series, axis=0)),
-                            dtype=np.float) / MC_SAMPLES
-        
-        win_frac = np.append(win_frac, [0.0 for x in range(len(mc_series) - 
-                                                           win_frac.shape[0])])
-        winner_idx = np.argmax(win_frac)
+            win_frac = np.array(np.bincount(np.argmax(mc_series, axis=0)),
+                                dtype=np.float) / MC_SAMPLES
+            
+            win_frac = np.append(win_frac, [0.0 for x in range(len(mc_series) - 
+                                                               win_frac.shape[0])])
+            winner_idx = np.argmax(win_frac)
 
-        # Determine the number of real impressions for each entry in win_frac
-        impressions = [x.get_impressions() for x in valid_bandits]
-        if non_exp_thumb is not None:
-            impressions.append(non_exp_thumb.get_impressions())
+            # Determine the number of real impressions for each entry in win_frac
+            impressions = [x.get_impressions() for x in valid_bandits]
+            if non_exp_thumb is not None:
+                impressions.append(non_exp_thumb.get_impressions())
 
-        # Determine the value remaining. This is equivalent to
-        # determing that one of the other arms might beat the winner
-        # by x%
-        lost_value = ((np.max(mc_series, 0) - mc_series[:][winner_idx]) / 
-                      mc_series[:][winner_idx])
-        value_remaining = np.sort(lost_value)[0.95*MC_SAMPLES]
+            # Determine the value remaining. This is equivalent to
+            # determing that one of the other arms might beat the winner
+            # by x%
+            lost_value = ((np.max(mc_series, 0) - mc_series[:][winner_idx]) / 
+                          mc_series[:][winner_idx])
+            value_remaining = np.sort(lost_value)[0.95*MC_SAMPLES]
 
-        # For all those thumbs that haven't been seen for 1000 imp,
-        # make sure that they will get some traffic
-        for i in range(len(valid_bandits)):
-            if impressions[i] < 500:
-                win_frac[i] = max(0.1, win_frac[i])
-        win_frac = win_frac / np.sum(win_frac)
+            # For all those thumbs that haven't been seen for 1000 imp,
+            # make sure that they will get some traffic
+            for i in range(len(valid_bandits)):
+                if impressions[i] < 500:
+                    win_frac[i] = max(0.1, win_frac[i])
+            win_frac = win_frac / np.sum(win_frac)
+
+        else: # is_exp_concluded == True
+            # Load the data
+            # Use the closed form of formulation to see if we get the p value with a different winner.
+            # check if the winner is the same as the one loaded.
+            if winner_idx != winner_complete:
+                # write the winner back to the data.
+                # update the winning time
+            # Reset the fractions basing on the percentage it calculated.
 
         if win_frac[winner_idx] >= 0.95:
             # There is a winner. See if there were enough imp to call it
