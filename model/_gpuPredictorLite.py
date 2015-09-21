d9c32dc7aa8181f93af5f56125c7e8029bd58062
Nick <dufour@neon-lab.com>
2015-09-21 14:23:29 -0700

    created new job manager classes

--- model/_gpuPredictorLite.py
+++ model/_gpuPredictorLite.py
@@ -53,7 +53,7 @@ class _GPUMgr(caffe.Net):
         caffe.Net.__init__(self, model_file, pretrained_file, caffe.TEST)
         in_ = self.inputs[0]
         self.image_dims = np.array(self.blobs[in_].data.shape[1:])
-        logging.debug('instantiated.')
+        logging.debug('Instantiated.')
 
     def __call__(self, data_array):
         '''
@@ -75,7 +75,227 @@ class _GPUMgr(caffe.Net):
         predictions = np.exp(out[self.outputs[0]])
         return list(predictions[:,0])
 
-class JobManager(multiprocessing.Process):
+class _Preprocess(object):
+    ''' 
+    preprocesses images so they are appropriate to be
+    fed into the GPU, and reorients their dimensions
+    as necessary. Note that images are either read in
+    in the openCV fashion (B, G, R order) or are provided
+    as such.
+    '''
+    def __init__(self, image_dims, image_mean=[104, 117, 123]):
+        '''
+        image_dims = W x H array / list / tuple
+        image_mean = triple of channel mean values 
+        '''
+        self.image_dims = image_dims
+        if image_mean == None:
+            image_mean = [0, 0, 0] 
+        self.image_mean = image_mean
+
+    def _read(self, imgfn):
+        '''
+        If img is provided as a filename, this will
+        read it in. 
+        '''
+        img = cv2.imread(imgfn)
+        if img == None:
+            raise ValueError("Could not find image %s"%(imgfn))
+        return img
+
+    def __call__(self, img):
+        '''
+        Actually performs the preprocessing
+        '''
+        logging.debug('Preprocessing image')
+        if type(img) == str:
+            img = self._read(img)
+        if not type(img).__module__ == np.__name__:
+            raise TypeError("Image must be a numpy array (or str filename)")
+        img = img.astype(np.float32)
+        img = cv2.resize(img, (self.image_dims[0], self.image_dims[1]))
+        if img.shape[2] == 1:
+            # it's a black and white image, we have to colorize it
+            img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+        elif img.shape[2] != 3:
+            raise ValueError("Image has the incorrect number of channels")
+        # subtract the channelwise image means
+        img -= self.image_mean
+        img = img.transpose(2, 0, 1)
+        return img
+
+class _Predictor(object):
+    '''
+    The Predictor class, which is handed off the video clients.
+    You should never need to call this yourself, instead they are
+    instantiated within JobManager.
+    '''
+    def __init__(self, putQ, getQ, cmax, prep,
+                 clients, dead_clients, valid_videos,
+                 client_has_died, terminate):
+        '''
+        putQ = the MP queue to put jobs into
+        getQ = the MP queue to get jobs from
+        max = the maximum number of jobs to allow
+        cid = the ID of this client
+        prep = the preprocessing function
+        clients = the global list of valid clients
+        dead_clients = the global list of dead clients
+        valid_videos = a list of valid videos
+        client_has_died = a condition that incidates that 
+                          this client has died.
+        terminate = an event that terminates all child
+                    processes of the JobManager.
+        '''
+        self._putQ = putQ
+        self._getQ = getQ
+        self._prep = prep
+        self.max = cmax
+        self.cid = id(self)
+        self._clients = clients
+        self._dead_clients = dead_clients
+        self._valid_videos = valid_videos
+        self._client_has_died = client_has_died
+        self._terminate = terminate
+        self._pending_requests = []
+        self._cur_video = cid # initialize to the client ID
+        self._results = []
+        self._get_result_thread = None
+        self._fully_init = False
+        self._total_jobs = 0
+
+    def _start_result(self):
+        '''
+        Locally initialize the results thread
+        '''
+        logging.debug('Starting the threads locally')
+        self._get_result_thread = threading.Thread(
+                target=self._get_result)
+        self._get_result_thread.daemon = True
+        self._get_result_thread.start()
+        self._fully_init = True
+
+    def predict(self, data, vid=None, jid=None):
+        '''
+        Asynchronously predicts data. if vid, the video
+        id, is None, then it uses the client id. If jid, 
+        the job ID, is None, then it uses the total jobs
+        submitted so far.
+
+        Jobs in the putQ have the form:
+        (ID, data)
+
+        where ID is 
+        (cid, vid, jid)
+        '''
+        if not self._fully_init:
+            logging.debug('Locally starting threads')
+            self._start_result()
+        if len(self._pending_requests) >= self.max:
+            logging.debug('Too many pending requests(%i)'%(
+                len(self._pending_requests)))
+            return
+            # don't submit the job!
+        if vid == None:
+            logging.debug('video id is undefined, assigning it to %i'%(
+                self._cur_video))
+            vid = self._cur_video
+        if jid == None:
+            logging.debug('job id is currently undefined, assigning it to %i'%(
+                self._total_jobs))
+            jid = self._total_jobs
+        self._check_vid(vid)
+        self._inQ.put((self.cid, vid, jid), 
+                      self.prep(data))
+        self._pending_requests.append(jid)
+        self._total_jobs += 1
+        logging.debug('%ith job submitted'%(self._total_jobs))
+
+    def _check_vid(self, vid):
+        '''
+        verifies that the video ID is the current one,
+        if not, retires the old one and makes appropriate
+        changes
+        '''
+        if self._cur_video != vid:
+            logging.debug('New video seen: %i vs. %i'%(vid,
+                self._cur_video))
+            try:
+                logging.debug('Attempting to remove from valid videos')
+                self._valid_videos.remove(self.cur_video)
+            except:
+                logging.debug('Invalid video id, although this may not be a problem')
+            self._valid_videos.append(vid)
+            self._cur_video = vid
+            # remove pending results
+            logging.debug('Purging %i results awaiting integration'%(
+                len(self._results)))
+            self._results = []
+            logging.debug('Purging %i pending jobs from jobs list'%(
+                len(self._pending)))
+            self._pending = []
+
+    def _get_result(self):
+        '''
+        A synchronously fetches results from the 
+        get Queue, which is populated by a thread
+        under the control of the video manager. 
+        Because processing is done asynchronously,
+        it's possible that it will return results
+        that are no longer applicable, in which
+        case they will be discarded.
+        '''
+        logging.debug('Results fetcher started')
+        while True:
+            logging.debug('Fetching finished results')
+            try:
+                item = self._getQ.get_nowait()
+            except:
+                continue
+            if self._terminate:
+                logging.debug('Termination order recieved!')
+                return
+            if item == None:
+                # this video client is terminating
+                logging.debug('This video client terminating')
+                return
+            else:
+                (cid, vid, jid), score = item
+                try:
+                    logging.debug('Attempting to remove job id from pending')
+                    self._pending_requests.remove(jid)
+                except:
+                    logging.debug('Invalid pending job ID')
+                if vid != self._cur_video:
+                    logging.debug('Obtained result corresponds to finished video')
+                    continue
+                self._results.append((vid, jid, score))
+
+    def results(self):
+        '''
+        Returns results
+        '''
+        return self._results
+
+    def stop(self):
+        '''
+        Locally terminate the results.
+        '''
+        logging.debug('Stopping...')
+        logging.debug('Enqueueing null result')
+        self._getQ.put(None)
+        logging.debug('Removing self from active clients')
+        self._clients.remove(self)
+        logging.debug('Adding self to dead clients list')
+        self._dead_clients.append(self)
+        logging.debug('Notifying JobManager')
+        with self._client_has_died:
+            self._client_has_died.notify_all()
+        logging.debug('Joining results thread')
+        self._get_result_thread.join()
+
+
+class JobManager(object):
     '''
     Simplified JobManager, which maintains several items:
     >> *Note: MMP = Manager Multiprocessing
@@ -86,51 +306,86 @@ class JobManager(multiprocessing.Process):
         - MP List of valid video IDs
         - MP List of valid client IDs
         - MP List of dead client IDs
+
+    Note that this JobManager is designed to run in the
+    main process, and further manages all GPU clients 
+    from within Main(). However, since it uses Managed
+    lists, this requires another process to take over as
+    the Manager server.
+
+    FOR NOW, this will only work with 1 GPU. 
     '''
     def __init__(self, model_file, pretrained_file,
                  batchSize=32, image_dims=[224,224,3],
-                 image_mean=[104,117,123], nGPUs=2,
-                 N=None):
+                 image_mean=[104,117,123], N=None):
+        logging.debug('Initializing')
         self._manager = multiprocess.Manager()
         self._clients = self._manager.list()
         self._dead_clients = self._manager.list()
         self._valid_videos = self._manager.list()
-        self._terminate_self = self._manager.Event()
+        self._terminate = self._manager.Event()
         self._client_has_died = self._manager.Condition() # wakes up the garbage collector thread
         self._output_queues = dict()  # client ID --> output pipeline (queue)
-        self._pending_counts = dict() # client ID --> # of jobs pending
+        self._pretrained = pretrained_file
+        self._model = model_file
+        self._batchSize = batchSize
+        self._image_dims = image_dims
+        self._image_mean = image_mean
+        self._gpu2mgr = threading.Queue()
+        if N == None:
+            N = np.inf
         self._max = N
-        self._mgr2gpu = Queue()
-        self._client2mgr = self._manager.Queue()
-        self._QM = _QueueManager(model_file, pretrained_file,
-                                self._inQ, batchSize, 
-                                image_dims, image_mean,
-                                timeout, nGPUs)
-        self._QM.start()
-        self._input = None
-        self._allocator = None
-        self._garbage_collector = None
+        self._client2mgr = Queue()
+        self._submit_thread = None
+        self._garbage_collect_thread = None
         self._start_threads()
+        logging.debug('Starting GPU manager')
+        self._gpu_mgr = _GPUMgr(self._model, self._pretrained)
+        self._prep = _Preprocess(self._image_dims, 
+                                 self._image_mean)
 
     def _start_threads(self):
         '''
         Initializes the allocator, garbage collector, and
         input handling threads.
         '''
+        self._submit_thread = threading.Thread(
+            target=self._submit)
+        self._submit_thread.daemon = True
+        self._garbage_collect_thread = threading.Thread(
+            target=self._garbage_collect)
+        self._garbage_collect_thread.daemon = True
+        logging.debug('Starting garbage collector daemon')
+        self._garbage_collect_thread.start()
+        logging.debug('Starting submission / allocation daemon')
+        self._submit_thread.start()
 
-    def register_client(self):
+    def register_client(self, cmax=self._max, cid=None):
         '''
         Register and returns a new _Predictor object
         '''
+        # instantiate a new queue
+        mgr2client = self._manager.Queue()
+        new_client = _Predictor(self.client2mgr, mgr2client,
+            cmax, self._prep, self._clients,
+            self._dead_clients, self._valid_videos,
+            self._client_has_died, self._terminate)
+        cid = id(new_client)
+        logging.debug('Predictor %i instantiated'%(cid))
+        self._output_queues[cid] = mgr2client 
+        self._clients.append(cid)
+        return new_client
 
-    def _prune_dead_clients(self):
+    def _garbage_collect(self):
         '''
         Destroys the dead client's output queue.
         '''
         self._client_has_died.acquire()
         while True:
             self._client_has_died.wait()
-            if self._terminate_self.is_set():
+            logging.debug('Notified of client death!')
+            if self._terminate.is_set():
+                logging.debug('Server is shutting down!')
                 return # you've been ordered to die
             while self._dead_clients:
                 dcq = self._dead_clients.pop()
@@ -138,35 +393,103 @@ class JobManager(multiprocessing.Process):
 
     def _submit(self):
         '''
-        Submits data into the qeueue
+        Submits data to the GPU, and allocates
+        the results back to the appopriate clients.
         '''
-        self._job_was_submitted.acquire()
+        logging.debug('Instantiating contiguous GPU data array')
+        gpuArray = np.ascontiguousarray(
+            np.zeros(self._batchSize, 
+                     self._image_dims[2],
+                     self._image_dims[0],
+                     self._image_dims[1]
+            ).astype(np.float32))
+        
+        def _grab(is_first):
+            if is_first:
+                item = self._client2mgr.get()
+                if self._terminate.is_set():
+                    return
+                return item
+            try:
+                item = self._client2mgr.get_nowait()
+                return item
+            except:
+                pass
+
+        def _grab_data():
+            '''
+            Gracefully grabs all available to data
+            that are in need of analysis
+            '''
+            pending = [] 
+            # grab the first job
+            while True:
+                logging.debug('Waiting on first job')
+                item = _grab(True)
+                if self._terminate.is_set():
+                    logging.debug('Received termination order!')
+                    return
+                client, video, jid = ID
+                if client not in self._valid_clients:
+                    logging.debug('Job request from dead/invalid client')
+                    continue
+                if video not in self._valid_videos:
+                    logging.debug('Job request for invalid / completed video')
+                    continue
+                pending.append(ID)
+                gpuArray[len(pending)-1,:,:,:] = data
+                break
+            # grab remaining data
+            while True:
+                if len(pending) >= self.batchSize:
+                    logging.debug('Batch is ready')
+                    return pending
+                item = _grab()
+                if self._terminate.is_set():
+                    logging.debug('Received termination order!')
+                    return
+                if item == None:
+                    logging.debug('Reached end of waiting jobs')
+                    break
+                if client not in self._valid_clients:
+                    logging.debug('Job request from dead/invalid client')
+                    continue
+                if video not in self._valid_videos:
+                    logging.debug('Job request for invalid / completed video')
+                    continue
+                ID, data = item
+                pending.append(ID)
+                gpuArray[len(pending)-1,:,:,:] = data
+            return pending
+
+
         while True:
-            self._job_was_submitted.wait()
-            if self._terminate_self.is_set():
+            pending = _grab_data()
+            if self._terminate.is_set():
+                logging.debug('Received termination order!')
                 return
-            ID, data = self._client2mgr.get_nowait()
-            client, video, jid = ID
-            if client not in self._valid_clients:
-                continue
-            if video not in self._valid_videos:
-                continue
-                
+            scores = self._gpu_mgr(gpuArray[:len(pending),:,:,:])
+            for sid, score in zip(pending, scores):
+                cid, vid, jid = sid
 
     def stop(self):
         '''
         Stops the associated threads, as well
         as all child predictors.
         '''
-        self._terminate_self.set()
+        logging.debug('Termination request initiated')
+        logging.debug('Bringing down job server')
+        self._terminate.set()
+        # terminate garbage collect
+        logging.debug('Notifying job collector')
         with self._client_has_died:
             self._client_has_died.notify_all()
-        with self._job_has_finished:
-            self._job_has_finished.notify_all()
-        with self._job_was_submitted:
-            self._job_was_submitted.notify_all()
-        self._QM.join()
-        self._input.join()
-        self._allocator.join()
-        self._garbage_collector.join()
+        # terminate submit -- in case it's waiting
+        # on a job.
+        logging.debug('Notifying submittor / allocator')
+        self._client2mgr.put(None)
+        logging.debug('Joining garbage collector')
+        self._garbage_collect_thread.join()
+        logging.debug('Joining submittor / allocator')
+        self._submit_thread.join()
 
