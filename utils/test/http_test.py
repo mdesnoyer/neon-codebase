81c20bba4e5042722adaf9e4eeac2db78fe05bc9
Mark Desnoyer <desnoyer@neon-lab.com>
2015-10-28 17:07:24 -0700

    Fix test

--- utils/test/http_test.py
+++ utils/test/http_test.py
@@ -55,6 +55,14 @@ class TestSendRequest(test_utils.neontest.AsyncTestCase):
 
         self.assertEqual(utils.http.send_request(request), response)
 
+    def test_invalid_url(self):
+        with self.assertLogExists(logging.ERROR, 'Invalid url to request'):
+            response = utils.http.send_request(
+                HTTPRequest('file:///some/local/file'))
+
+        self.assertEquals(response.code, 400)
+        self.assertIsNotNone(response.error)
+
     def test_json_error_field(self):
         request, valid_response = create_valid_ack()
         invalid_response = HTTPResponse(request, 200,
