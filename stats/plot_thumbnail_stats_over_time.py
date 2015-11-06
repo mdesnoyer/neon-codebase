4262a9c42c5d4d96dfb3fe3f9bccec00a0d85e84
Mark Desnoyer <desnoyer@neon-lab.com>
2015-11-05 17:56:49 -0800

    Add script to make graphs of hbase data

--- stats/plot_thumbnail_stats_over_time.py
+++ stats/plot_thumbnail_stats_over_time.py
@@ -28,7 +28,7 @@ import stats.metrics
 import utils.neon
 from utils.options import options, define
 
-define("stats_host", default="54.210.126.245",
+define("stats_host", default="localhost",
         type=str, help="Host to connect to the stats db on.")
 define("stats_port", default=21050, type=int,
        help="Port to connect to the stats db on")
