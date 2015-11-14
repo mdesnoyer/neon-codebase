203bf6648688791e088bc803042a60a92becadce
Nick <dufour@neon-lab.com>
2015-11-14 14:21:37 -0800

    thumbs are automatically passed if they have minimum distance from other top thumbs of at least 0.20 or are in the 50th percentile of the average distance

--- model/local_video_searcher.py
+++ model/local_video_searcher.py
@@ -596,7 +596,7 @@ class LocalSearcher(object):
         self.col_stat = ColorStatistics()
         # define the variation measures and requirements
         f_min_var_acc = lambda: max(0.05, self.col_stat.percentile(3.))
-        f_max_var_rej = lambda: max(0.10, self.col_stat.percentile(30.))
+        f_max_var_rej = lambda: min(0.20, self.col_stat.percentile(50.))
         self.n_thumbs = n
         self.results = ResultsList(n_thumbs=n, min_acceptable=f_min_var_acc,
                                    max_rejectable=f_max_var_rej)
