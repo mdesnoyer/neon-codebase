354d84f724eae2969c5388484b25c0ff8446d072
Nick Dufour <ndufour@eunoia.local>
2015-11-16 01:33:25 -0800

    increased the processing time ratio to see if it makes things better

--- model/LOCAL_SEARCH_TEST_AND_EXAMPLE.py
+++ model/LOCAL_SEARCH_TEST_AND_EXAMPLE.py
@@ -200,6 +200,7 @@ LS = LocalSearcher(predictor, face_finder, eye_scorer,
                    feats_to_cache=feats_to_cache,
                    testing=True,
                    comb_score_weight=1.,
+                   processing_time_ratio=2.0,
                    testing_dir='/data/local_search/testing')
 # this shouldn't be warning but I want to test it out
 # _log.info('Testing DILL')
