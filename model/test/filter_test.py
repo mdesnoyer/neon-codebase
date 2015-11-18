a70edcc3ce7155e0687e1da5c93659407e4156a4
Nick <dufour@neon-lab.com>
2015-11-17 16:07:58 -0800

    create a unittests for pixel variance filter and scene change filter

--- model/test/filter_test.py
+++ model/test/filter_test.py
@@ -7,10 +7,13 @@ if sys.path[0] <> base_path:
 
 import unittest
 import model.filters
+import model.features
 import numpy as np
 import cv2
 
 IMG_DIR = os.path.join(os.path.dirname(__file__), 'test_filter_images')
+SCENE_CHANGE_DIR = os.path.join(os.path.dirname(__file__),
+                                'test_scene_change_images')
 
 class TestUniformColorFilter(unittest.TestCase):
     def setUp(self):
@@ -110,6 +113,40 @@ class TestTextFilter(unittest.TestCase):
         #    cv2.imread(os.path.join(IMG_DIR, 'hgtv.jpg'))))
         self.assertFalse(self.filter.accept(
             cv2.imread(os.path.join(IMG_DIR, 'text_and_pics.jpg'))))
-        
+
+# add tests for PixVar
+class TestPixelVarianceFilter(unittest.TestCase):
+    def setUp(self):
+        self.filter = model.filters.ThreshFilt(thresh=200)
+        self.feature = model.features.PixelVarGenerator()
+
+    def test_pixvar_filter(self):
+        self.assertFalse(self.filter.filter(
+            cv2.imread(os.path.join(IMG_DIR, 'white_middle_garbage.jpg'))))
+        self.assertFalse(self.filter.filter(
+            cv2.imread(os.path.join(IMG_DIR, 'black.jpg'))))
+        self.assertFalse(self.filter.filter(
+            cv2.imread(os.path.join(IMG_DIR, 'white_logo.jpg'))))
+
+# add tests for SceneChange
+class TestSceneChangeDetection(unittest.TestCase):
+    def setUp(self):
+        self.filter = model.filters.SceneChangeFilter(mean_mult=2., std_mult=1.5)
+        self.feature = model.features.SADGenerator()
+
+    def test_scene_change(self):
+        images = [cv2.imread(x) for x in glob(
+            os.path.join(SCENE_CHANGE_DIR, '*.jpg'))]
+        feats = self.feature.generate_many(images)
+        filtd = self.filter.filter(feats)
+        self.assertTrue(filtd[0])
+        self.assertTrue(filtd[1])
+        self.assertTrue(filtd[2])
+        self.assertTrue(filtd[3])
+        self.assertFalse(filtd[4])
+        self.assertFalse(filtd[5])
+        self.assertTrue(filtd[6])
+        self.assertTrue(filtd[7])
+
 if __name__ == '__main__':
     unittest.main()
