2302b01140cdc55d46aa291395573b704f36211e
Kevin Fenger <fenger@neon-lab.com>
2015-11-24 14:16:42 -0800

    fixing cnn api test

--- api/test/cnn_api_test.py
+++ api/test/cnn_api_test.py
@@ -22,15 +22,19 @@ import utils.neon
 class TestCNNApi(test_utils.neontest.AsyncTestCase):
     def setUp(self):
         super(TestCNNApi, self).setUp()
-        self.redis = test_utils.redis.RedisServer()
-        self.redis.start() 
+        self.cnn_api_mocker = patch('api.cnn_api.CNNApi.search')
+        self.cnn_api_mock = self._future_wrap_mock(self.cnn_api_mocker.start()) 
 
     def tearDown(self):
-        self.redis.stop()
-    
+        self.cnn_api_mocker.stop()
+        super(TestCNNApi, self).tearDown()
+ 
     @tornado.testing.gen_test
     def test_base_search(self):
         c = api.cnn_api.CNNApi('c2vfn5fb8gubhrmd67x7bmv9')
-        blah = yield c.search(parse('2015-10-29T00:00:00Z')) 
-        import pdb; pdb.set_trace()
-        
+        self.cnn_api_mock.side_effect = ['{ somejson: json }']
+        yield c.search(parse('2015-10-29T00:00:00Z'))
+        cargs_list = self.cnn_api_mock.call_args_list
+        self.assertEquals(cargs_list[0][0][0].year, 2015) 
+        self.assertEquals(cargs_list[0][0][0].month, 10) 
+        self.assertEquals(cargs_list[0][0][0].day, 29) 
