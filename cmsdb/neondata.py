b7779511770da51c2a12173170a35106518187b2
Kevin Fenger <fenger@neon-lab.com>
2015-12-02 13:08:01 -0800

    some bug fixes to fix video submit

--- cmsdb/neondata.py
+++ cmsdb/neondata.py
@@ -1858,7 +1858,7 @@ class InternalVideoID(object):
     ''' Internal Video ID Generator '''
     NOVIDEO = 'NOVIDEO' # External video id to specify that there is no video
 
-    VALID_EXTERNAL_REGEX = '[0-9a-zA-Z\-\.]+'
+    VALID_EXTERNAL_REGEX = '[0-9a-zA-Z\-\.~]+'
     VALID_INTERNAL_REGEX = ('[0-9a-zA-Z]+_%s' % VALID_EXTERNAL_REGEX)
     
     @staticmethod
