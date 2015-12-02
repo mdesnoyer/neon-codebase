34a3a0f525b36c9b1b355bc05d8c9b98336356c0
Nick <dufour@neon-lab.com>
2015-12-02 15:51:02 -0800

    fixed bug (crop_source --> source crop)

--- cmsdb/cdnhosting.py
+++ cmsdb/cdnhosting.py
@@ -121,7 +121,7 @@ class CDNHosting(object):
         self.update_serving_urls = cdn_metadata.update_serving_urls
         self.rendition_sizes = cdn_metadata.rendition_sizes or []
         self.cdn_prefixes = cdn_metadata.cdn_prefixes
-        self.crop_source = cdn_metadata.crop_source
+        self.source_crop = cdn_metadata.source_crop
 
     @utils.sync.optional_sync
     @tornado.gen.coroutine
@@ -149,13 +149,13 @@ class CDNHosting(object):
         Returns: list [(cdn_url, width, height)]
         '''
         new_serving_thumbs = [] # (url, width, height)
-        if self.crop_source is not None:
+        if self.source_crop is not None:
             if not self.resize:
                 _log.error(('Crop source specified but no desired final size '
                             'is defined'))
                 raise ValueError(('Crop source can only operate along with '
                                   'resize'))
-            _prep = pycvutils.ImagePrep(crop_frac=self.crop_source)
+            _prep = pycvutils.ImagePrep(crop_frac=self.source_crop)
             image = _prep(image)
         # NOTE: if _upload_impl returns None, the image is not added to the 
         # list of serving URLs
