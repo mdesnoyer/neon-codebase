306b716215cff4fdcd72f5e2eac00238c2fbf9c9
Mark Desnoyer <desnoyer@neon-lab.com>
2016-01-21 18:39:59 -0800

    Import missing

--- tools/force_thumb.py
+++ tools/force_thumb.py
@@ -20,13 +20,13 @@ from utils.options import define, options
 
 _log = logging.getLogger(__name__)
 
-define('keep_default', default=0, help='if 1, keep the default thumb on')
+define('keep_default', default=1, help='if 1, keep the default thumb on')
 
 def main(thumb_ids):
     for thumb_id in thumb_ids:
         _log.info('Forcing thumb %s' % thumb_id)
-        keep_thumb = ThumbnailMetadata.get(thumb_id)
-        video = VideoMetadata.get(keep_thumb.video_id)
+        keep_thumb = neondata.ThumbnailMetadata.get(thumb_id)
+        video = neondata.VideoMetadata.get(keep_thumb.video_id)
 
         def _disable_thumbs(objs):
             for thumb in objs.itervalues():
@@ -36,8 +36,8 @@ def main(thumb_ids):
                 if thumb.key == keep_thumb.key:
                     continue
                 thumb.enabled = False
-        thumbs = ThumbnailMetadata.modify_many(video.thumbnail_ids,
-                                               _disable_thumbs)
+        thumbs = neondata.ThumbnailMetadata.modify_many(video.thumbnail_ids,
+                                                        _disable_thumbs)
 
 if __name__ == "__main__":
     args = utils.neon.InitNeon(USAGE)
