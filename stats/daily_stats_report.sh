fd5975872aa6e2526d95124f37897ce1b2d95a74
Mark Desnoyer <desnoyer@neon-lab.com>
2016-03-16 17:48:42 -0700

    Fix bug

--- stats/daily_stats_report.sh
+++ stats/daily_stats_report.sh
@@ -6,7 +6,7 @@ source enable_env
 TODAY=`date -I -u`
 YESTERDAY=`date -I -u -d "yesterday"`
 
-COMMON_PARAMS='--start_time ${YESTERDAY} --end_time ${TODAY} --cmsdb.neondata.wants_postgres 1 --cmsdb.neondata.db_user readonly_user --cmsdb.neondata.db_password e73zfjiRO3Mr --cmsdb.neondata.db_address postgres-prod.cnvazyzlgq2v.us-east-1.rds.amazonaws.com --stats.cluster.cluster_name "Neon Event Cluster"'
+COMMON_PARAMS="--start_time ${YESTERDAY} --end_time ${TODAY} --cmsdb.neondata.wants_postgres 1 --cmsdb.neondata.db_user readonly_user --cmsdb.neondata.db_password e73zfjiRO3Mr --cmsdb.neondata.db_address postgres-prod.cnvazyzlgq2v.us-east-1.rds.amazonaws.com --stats.cluster.cluster_name \"Neon Event Cluster\""
 
 # IGN
 stats/generate_analytics_report_at_winner_lift.py --pub_id 2089095449 --baseline_types default,customupload ${COMMON_PARAMS} --min_impressions 500 --output ~/reports/ign_${YESTERDAY}_${TODAY}.xls 
