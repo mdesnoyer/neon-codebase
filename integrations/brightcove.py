a1ba39935d6fff6ace08b81bef34816dc9a9a5da
Nate Kresge <kresge@neon-lab.com>
2016-03-28 14:19:00 -0700

    pep8 cleanup

--- integrations/brightcove.py
+++ integrations/brightcove.py
@@ -19,13 +19,13 @@ import logging
 import re
 import tornado.gen
 import urlparse
-from utils.options import options, define
+from utils.options import define
 from utils import statemon
 
-define('max_vids_for_new_account', default=100, 
+define('max_vids_for_new_account', default=100,
        help='Maximum videos to process for a new account')
 
-define('max_submit_retries', default=3, 
+define('max_submit_retries', default=3,
        help='Maximum times we will retry a video submit before passing on it.')
 
 statemon.define('bc_apiserver_errors', int)
@@ -40,6 +40,7 @@ statemon.define('old_videos_skipped', int)
 
 _log = logging.getLogger(__name__)
 
+
 def _normalize_thumbnail_url(url):
     '''Returns a thumb url without transport mechanism or query string.'''
     if url is None:
@@ -52,6 +53,7 @@ def _normalize_thumbnail_url(url):
         return 'brightcove.com/%s' % (os.path.basename(parse.path))
     return '%s%s' % (parse.netloc, parse.path)
 
+
 def _get_urls_from_bc_response(response):
     urls = []
     for image_type in ['thumbnail', 'videoStill']:
@@ -60,6 +62,7 @@ def _get_urls_from_bc_response(response):
             urls.append(response[image_type].get('remoteUrl', None))
 
     return [x for x in urls if x is not None]
+
 
 def _extract_image_info_from_bc_response(response, field):
     '''Extracts a list of fields from the images in the response.'''
@@ -71,6 +74,7 @@ def _extract_image_info_from_bc_response(response, field):
 
     return [unicode(x) for x in vals if x is not None]
 
+
 class BrightcoveIntegration(integrations.ovp.OVPIntegration):
     def __init__(self, account_id, platform):
         super(BrightcoveIntegration, self).__init__(account_id, platform)
@@ -79,8 +83,8 @@ class BrightcoveIntegration(integrations.ovp.OVPIntegration):
             self.platform.read_token, self.platform.write_token)
         self.is_platform_dual_keyed = True
         self.skip_old_videos = False
-        self.needs_platform_videos = True 
-        self.neon_api_key = self.platform.neon_api_key  
+        self.needs_platform_videos = True
+        self.neon_api_key = self.platform.neon_api_key
 
     @staticmethod
     def get_submit_video_fields():
@@ -104,7 +108,7 @@ class BrightcoveIntegration(integrations.ovp.OVPIntegration):
         '''Return a list of custom fields to request.'''
         custom_fields = None
         if self.platform.id_field not in [
-                neondata.BrightcovePlatform.REFERENCE_ID, 
+                neondata.BrightcovePlatform.REFERENCE_ID,
                 neondata.BrightcovePlatform.BRIGHTCOVE_ID]:
             custom_fields = [self.platform.id_field]
 
@@ -112,20 +116,20 @@ class BrightcoveIntegration(integrations.ovp.OVPIntegration):
 
     def _get_video_url_to_download(self, b_json_item):
         '''
-        Return a video url to download from a brightcove json item 
-        
-        if frame_width is specified, get the closest one  
+        Return a video url to download from a brightcove json item
+
+        if frame_width is specified, get the closest one
         '''
 
-        video_urls = [] # (width, encoding_rate, url)
+        video_urls = []  # (width, encoding_rate, url)
         try:
-            d_url  = b_json_item['FLVURL']
-        except KeyError, e:
+            d_url = b_json_item['FLVURL']
+        except KeyError:
             _log.error("missing flvurl %s" % b_json_item)
             return None
 
-        #If we get a broken response from brightcove api
-        if not b_json_item.has_key('renditions'):
+        # If we get a broken response from brightcove api
+        if 'renditions' not in b_json_item:
             return d_url
 
         renditions = b_json_item['renditions']
@@ -139,24 +143,24 @@ class BrightcoveIntegration(integrations.ovp.OVPIntegration):
                 video_urls.append(((rend["frameWidth"] or -1),
                                    (rend["encodingRate"] or -1),
                                   rend['remoteUrl']))
-       
+
         # no renditions
         if len(video_urls) < 1:
             return d_url
-        
+
         if self.platform.rendition_frame_width:
             # Get the highest encoding rate at a size closest to this width
             video_urls = sorted(video_urls, key=lambda x:
-                                (abs(x[0]-self.platform.rendition_frame_width),
+                                (abs(x[0] - self.platform.rendition_frame_width),
                                  -x[1]))
         else:
-            #return the max width rendition with the highest encoding rate
+            # return the max width rendition with the highest encoding rate
             video_urls = sorted(video_urls, key=lambda x: (-x[0], -x[1]))
         return video_urls[0][2]
 
     @staticmethod
     def _get_best_image_info(data):
-        '''Returns the (url, {image_struct}) of the best image in the 
+        '''Returns the (url, {image_struct}) of the best image in the
         Brightcove video object
         '''
         url = data.get('videoStillURL', None)
@@ -169,7 +173,6 @@ class BrightcoveIntegration(integrations.ovp.OVPIntegration):
             obj = data['videoStill']
 
         return url, obj
-        
 
     @tornado.gen.coroutine
     def lookup_and_submit_videos(self, ovp_video_ids, continue_on_error=False):
@@ -193,9 +196,9 @@ class BrightcoveIntegration(integrations.ovp.OVPIntegration):
             _log.error('Client error getting data from Brightcove for '
                        'platform %s: %s' % (self.platform.get_id(), e))
             raise integrations.ovp.OVPError(e)
-            
-        self.set_video_iter_with_videos(bc_video_info) 
-        retval = yield self.submit_ovp_videos(self.get_next_video_item_playlist, 
+
+        self.set_video_iter_with_videos(bc_video_info)
+        retval = yield self.submit_ovp_videos(self.get_next_video_item_playlist,
                                               continue_on_error=continue_on_error)
 
         raise tornado.gen.Return(retval)
@@ -205,7 +208,7 @@ class BrightcoveIntegration(integrations.ovp.OVPIntegration):
         '''Sets the Brightcove thumbnail that is returned from their apis.'''
         # Not implemented for now because it's not needed
         raise NotImplementedError()
-    
+
         # This will use update_thumbnail_and_videostill in brightcove
         # api and make changes to our database as necessary.
 
@@ -214,7 +217,7 @@ class BrightcoveIntegration(integrations.ovp.OVPIntegration):
         '''Submits any playlist videos for this account.
 
         Returns: dictionary of video_id => job_id
-        '''                
+        '''
         retval = {}
         for playlist_id in self.platform.playlist_feed_ids:
             try:
@@ -227,18 +230,17 @@ class BrightcoveIntegration(integrations.ovp.OVPIntegration):
             except brightcove_api.BrightcoveApiServerError as e:
                 statemon.state.increment('bc_apiserver_errors')
                 _log.error('Server error getting playlist %s from '
-                           'Brightcove for platform %s: %s' % 
+                           'Brightcove for platform %s: %s' %
                            (playlist_id, self.platform.get_id(), e))
                 raise integrations.ovp.OVPError(e)
             except brightcove_api.BrightcoveApiClientError as e:
                 statemon.state.increment('bc_apiclient_errors')
                 _log.error('Client error getting playlist %s from '
-                           'Brightcove for platform %s: %s' % 
+                           'Brightcove for platform %s: %s' %
                            (playlist_id, self.platform.get_id(), e))
                 raise integrations.ovp.OVPError(e)
-            
- 
-            self.set_video_iter_with_videos(cur_results['videos']) 
+
+            self.set_video_iter_with_videos(cur_results['videos'])
             cur_jobs = yield self.submit_ovp_videos(self.get_next_video_item_playlist)
             retval.update(cur_jobs)
 
@@ -248,16 +250,16 @@ class BrightcoveIntegration(integrations.ovp.OVPIntegration):
     def submit_new_videos(self):
         '''Submits new videos in the account.'''
         self.skip_old_videos = True
-        self.set_video_iter()  
-        video_tuple_list = yield self.submit_ovp_videos(self.get_next_video_item_normal)
-    
+        self.set_video_iter()
+        yield self.submit_ovp_videos(self.get_next_video_item_normal)
+
     @tornado.gen.coroutine
     def process_publisher_stream(self):
         yield self.submit_playlist_videos()
         yield self.submit_new_videos()
 
     def get_video_id(self, video):
-        '''override from ovp''' 
+        '''override from ovp'''
         # Get the video id to use to key this video
         if self.platform.id_field == neondata.BrightcovePlatform.REFERENCE_ID:
             video_id = video.get('referenceId', None)
@@ -267,7 +269,7 @@ class BrightcoveIntegration(integrations.ovp.OVPIntegration):
                 statemon.state.increment('cant_get_refid')
                 _log.error_n(msg)
                 raise integrations.ovp.OVPRefIDError(msg)
-        elif (self.platform.id_field == 
+        elif (self.platform.id_field ==
               neondata.BrightcovePlatform.BRIGHTCOVE_ID):
             video_id = video['id']
         else:
@@ -283,31 +285,31 @@ class BrightcoveIntegration(integrations.ovp.OVPIntegration):
                 statemon.state.increment('cant_get_custom_id')
                 raise integrations.ovp.OVPCustomRefIDError(msg)
         return video_id
- 
+
     def get_video_url(self, video):
-        '''override from ovp''' 
+        '''override from ovp'''
         return self._get_video_url_to_download(video)
 
     def get_video_callback_url(self, video):
-        '''override from ovp''' 
+        '''override from ovp'''
         return self.platform.callback_url
 
     def get_video_title(self, video):
         '''override from ovp'''
-        video_title = video.get('name', '') 
-        return unicode(video_title) 
+        video_title = video.get('name', '')
+        return unicode(video_title)
 
     def get_video_custom_data(self, video):
-        '''override from ovp''' 
+        '''override from ovp'''
         custom_data = video.get('customFields', {})
         custom_data['_bc_int_data'] = {
-            'bc_id' : video['id'],
-            'bc_refid' : video.get('referenceId', None)
-            }
+            'bc_id': video['id'],
+            'bc_refid': video.get('referenceId', None)
+        }
         return custom_data
 
     def get_video_duration(self, video):
-        '''override from ovp''' 
+        '''override from ovp'''
         return float(video['length']) / 1000.0
 
     def get_video_publish_date(self, video):
@@ -317,26 +319,26 @@ class BrightcoveIntegration(integrations.ovp.OVPIntegration):
             publish_date = datetime.datetime.utcfromtimestamp(
                 int(publish_date) / 1000.0)
         return publish_date
- 
+
     def get_video_last_modified_date(self, video):
-        try: 
-            return int(video['lastModifiedDate']) / 1000.0 
+        try:
+            return int(video['lastModifiedDate']) / 1000.0
         except TypeError:
-            return None  
+            return None
 
     def get_video_thumbnail_info(self, video):
-        '''override from ovp''' 
+        '''override from ovp'''
         thumb_url, thumb_data = BrightcoveIntegration._get_best_image_info(video)
         thumb_ref = None
         if thumb_data is not None:
             thumb_ref = unicode(thumb_data['id'])
-        return { 'thumb_url' : thumb_url, 
-                 'thumb_ref' : thumb_ref }
+        return {'thumb_url': thumb_url,
+                'thumb_ref': thumb_ref}
 
     def set_video_iter(self):
         from_date = datetime.datetime(1980, 1, 1)
-        if (self.platform.last_process_date is not None and 
-            not self.platform.uses_batch_provisioning):
+        if (self.platform.last_process_date is not None and
+                not self.platform.uses_batch_provisioning):
             from_date = datetime.datetime.utcfromtimestamp(
                 self.platform.last_process_date)
 
@@ -347,10 +349,10 @@ class BrightcoveIntegration(integrations.ovp.OVPIntegration):
                 sort_order='ASC',
                 video_fields=BrightcoveIntegration.get_submit_video_fields(),
                 custom_fields=self.get_custom_fields())
-    
+
     def set_video_iter_with_videos(self, videos):
-        self.video_iter = iter(videos) 
-  
+        self.video_iter = iter(videos)
+
     @tornado.gen.coroutine
     def get_next_video_item_normal(self):
         try:
@@ -360,34 +362,34 @@ class BrightcoveIntegration(integrations.ovp.OVPIntegration):
         except brightcove_api.BrightcoveApiServerError as e:
             statemon.state.increment('bc_apiserver_errors')
             _log.error('Server error getting new videos from '
-                       'Brightcove for platform %s: %s' % 
+                       'Brightcove for platform %s: %s' %
                        (self.platform.get_id(), e))
             raise integrations.ovp.OVPError(e)
         except brightcove_api.BrightcoveApiClientError as e:
             statemon.state.increment('bc_apiclient_errors')
             _log.error('Client error getting new videos from '
-                       'Brightcove for platform %s: %s' % 
+                       'Brightcove for platform %s: %s' %
                        (self.platform.get_id(), e))
             raise integrations.ovp.OVPError(e)
-  
+
         raise tornado.gen.Return(item)
- 
+
     @tornado.gen.coroutine
     def get_next_video_item_playlist(self):
-        video = None 
-        try:  
+        video = None
+        try:
             video = self.video_iter.next()
-        except StopIteration: 
+        except StopIteration:
             video = StopIteration('hacky')
-  
-        raise tornado.gen.Return(video)  
+
+        raise tornado.gen.Return(video)
 
     def skip_old_video(self, publish_date, video_id):
         rv = False
-        if (self.skip_old_videos and 
-            publish_date is not None and 
+        if (self.skip_old_videos and
+            publish_date is not None and
             self.platform.oldest_video_allowed is not None and
-            publish_date < 
+            publish_date <
             dateutil.parser.parse(self.platform.oldest_video_allowed)):
             _log.info('Skipped video %s from account %s because it is too'
                       ' old' % (video_id, self.platform.neon_api_key))
@@ -395,8 +397,8 @@ class BrightcoveIntegration(integrations.ovp.OVPIntegration):
             rv = True
         return rv
 
-    def does_video_exist(self, video_meta, video_ref): 
-        return video_ref in self.platform.videos   
+    def does_video_exist(self, video_meta, video_ref):
+        return video_ref in self.platform.videos
 
     @tornado.gen.coroutine
     def _update_video_info(self, data, bc_video_id, job_id):
@@ -420,7 +422,7 @@ class BrightcoveIntegration(integrations.ovp.OVPIntegration):
         def _update_publish_date(x):
             x.publish_date = publish_date
             x.job_id = job_id
-        video = yield tornado.gen.Task(
+        yield tornado.gen.Task(
             neondata.VideoMetadata.modify,
             video_id,
             _update_publish_date)
@@ -432,7 +434,7 @@ class BrightcoveIntegration(integrations.ovp.OVPIntegration):
         yield tornado.gen.Task(
             neondata.NeonApiRequest.modify,
             job_id, self.platform.neon_api_key, _update_request)
-        
+
     @tornado.gen.coroutine
     def _grab_new_thumb(self, data, bc_video_id):
         '''Grab a new thumbnail from a video object if there is one.
@@ -442,17 +444,18 @@ class BrightcoveIntegration(integrations.ovp.OVPIntegration):
         bc_video_id - The brightcove video id
         '''
         thumb_url, thumb_data = \
-          BrightcoveIntegration._get_best_image_info(data)
+            BrightcoveIntegration._get_best_image_info(data)
         if thumb_data is None:
             _log.warn_n('Could not find thumbnail in %s' % data)
-            return 
-        bc_urls = [_normalize_thumbnail_url(x) for 
+            return
+        bc_urls = [_normalize_thumbnail_url(x) for
                    x in _get_urls_from_bc_response(data)]
 
         # Function that will set the external id in the ThumbnailMetadata
         external_id = thumb_data.get('id', None)
         if external_id is not None:
             external_id = unicode(external_id)
+
         def _set_external_id(obj):
             obj.external_id = external_id
 
@@ -481,9 +484,8 @@ class BrightcoveIntegration(integrations.ovp.OVPIntegration):
                     neondata.ThumbnailMetadata.modify,
                     thumb.key,
                     _set_type)
-            
-            if (thumb is None or 
-                thumb.type != neondata.ThumbnailType.DEFAULT):
+
+            if (thumb is None or thumb.type != neondata.ThumbnailType.DEFAULT):
                 continue
 
             if thumb.rank < min_rank:
@@ -492,8 +494,8 @@ class BrightcoveIntegration(integrations.ovp.OVPIntegration):
             if thumb.external_id is not None:
                 # We know about this thumb was in Brightcove so see if it
                 # is still there.
-                if (unicode(thumb.external_id) in 
-                    _extract_image_info_from_bc_response(data, 'id')):
+                if (unicode(thumb.external_id) in
+                        _extract_image_info_from_bc_response(data, 'id')):
                     found_thumb = True
             elif thumb.refid is not None:
                 # For legacy thumbs, we specified a reference id. Look for it
@@ -507,11 +509,11 @@ class BrightcoveIntegration(integrations.ovp.OVPIntegration):
             else:
                 # We do not have the id for this thumb, so see if we
                 # can match the url.
-                norm_urls = set([_normalize_thumbnail_url(x) 
+                norm_urls = set([_normalize_thumbnail_url(x)
                                  for x in thumb.urls])
                 if len(norm_urls.intersection(bc_urls)) > 0:
                     found_thumb = True
-                    
+
                     # Now update the external id because we didn't
                     # know about it before.
                     yield tornado.gen.Task(neondata.ThumbnailMetadata.modify,
@@ -528,16 +530,16 @@ class BrightcoveIntegration(integrations.ovp.OVPIntegration):
                     new_thumb = neondata.ThumbnailMetadata(
                         None,
                         ttype=neondata.ThumbnailType.DEFAULT,
-                        rank = min_rank-1,
-                        external_id = external_id
-                        )
+                        rank=min_rank - 1,
+                        external_id=external_id
+                    )
 
                     yield vid_meta.download_and_add_thumbnail(
                         new_thumb,
                         url,
                         save_objects=False,
                         async=True)
-                    
+
                     # Validate the new_thumb key exists or not. We noticed that
                     # Brightcove can send the same thumbnail with different
                     # external ids multiple times. This triggers the same
@@ -587,4 +589,3 @@ class BrightcoveIntegration(integrations.ovp.OVPIntegration):
                 _log.error('Could not find valid image to add to video %s. '
                            'Tried urls %s' % (vid_meta.key, urls))
                 statemon.state.increment('cant_get_image')
-        
