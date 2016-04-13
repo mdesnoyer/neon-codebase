4a2f7d6b3f88383bf0f3ca54d33ee16ae104cc88
Nate Kresge <kresge@neon-lab.com>
2016-04-13 13:55:26 -0700

    Implement video play percent

--- clickTracker/neon-videojs/src/plugin.js
+++ clickTracker/neon-videojs/src/plugin.js
@@ -6,9 +6,14 @@ import reqwest from 'reqwest';
 // Request basic events from the html5 component
 //const VIDEO_EVENTS = videojs.getComponent('Html5').Events;
 
+// Consider throttling
+
 // Tracking defaults for the plugin
 const defaults = {
+
+    // Default Neon api endpoint
     'trackUrl': 'http://tracker.neon-images.com/v2/track/',
+
     // Default events to remote log to Neon
     'trackEvents': [
         'image_load',
@@ -16,8 +21,11 @@ const defaults = {
         'image_click',
         'play',
         'ad_play',
-        'video_play_percent',
-    ]
+        'timeupdate',
+    ],
+
+    // Interval in percent to send video play percent
+    'timeupdateInterval': 25
 };
 
 // Mapping of Brightcove event type to Neon Api endpoint
@@ -28,18 +36,21 @@ const constants = {
         'image_click': 'ic',
         'play': 'vp',
         'ad_play': 'ap',
-        'video_play_percent': 'vpp',
+        'timeupdate': 'vpp',
     }
 };
 
 // Unimplemented data fields in the Neon payload
 const dummyParams = {
-    'pageid': 'ajsidfh823u9',
+    // Neon's publisher id
     'tai': '56139847',
+    // Tracker yype
     'ttype': 'BRIGHTCOVE',
+    // Url of event
     'page': 'http%3A%2F%2Fwww.neon-lab.com%2Fproduct',
-    'rts': 1406569946,
+    // Video identifier
     'vid': 'alskdjf987',
+    // 1-based count of videos played on this pageload
     'pcount': 1
 }
 
@@ -52,12 +63,15 @@ const dummyParams = {
 const onPlayerReady = (player, options) => {
     console.log('onPlayerReady');
     player.neon.options = options || {};
+    player.neon.pageId = _uuid();
+    player.neon.percentsPlayed = [];
     player.on('image_load', trackImageLoad);
-//    player.on('image_view', trackImageView);
-//    player.on('image_click', trackImageClick);
+    player.on('image_view', trackImageView);
+    player.on('image_click', trackImageClick);
     player.on('play', trackPlay);
-//    player.on('ad_play', trackAdPlay);
-//    player.on('video_play_percent', trackVideoPlayPercent);
+    player.on('ad_play', trackAdPlay);
+    player.on('timeupdate', trackVideoPlayPercent);
+
 };
 
 /**
@@ -91,12 +105,29 @@ const trackAdPlay = (playerEvent) => {
     _commonTrack(playerEvent);
 }
 
+const _getPercentPlayed = () => {
+    var currentTime, duration;
+    currentTime = Math.round(player.currentTime());
+    duration = Math.round(player.duration());
+    return Math.round(currentTime / duration * 100);
+}
+
 const trackVideoPlayPercent = (playerEvent) => {
-    _commonTrack(playerEvent);
+    var interval, percent
+    interval = Math.min(100, player.neon.options.timeupdateInterval);
+    percent = 0;
+
+    for(;percent <= 100; percent += interval) {
+        if(_getPercentPlayed() > percent) {
+            if(player.neon.percentsPlayed.indexOf(percent) < 0) {
+                player.neon.percentsPlayed.push(percent);
+                _commonTrack(playerEvent, {'prcnt': percent});
+            }
+        }
+    }
 }
 
 const _commonTrack = (playerEvent, extra) => {
-    console.log('player emit ' + playerEvent.type);
     extra = extra || {};
     if(player.neon.options.trackEvents.indexOf(playerEvent.type) >= 0) {
         remoteLogEvent(playerEvent.type, extra);
@@ -106,7 +137,13 @@ const _commonTrack = (playerEvent, extra) => {
 const remoteLogEvent = (eventType, extra) => {
 
     let action = constants['eventCodeMap'][eventType];
-    let data = videojs.mergeOptions(dummyParams, {'a': action});
+    let data = videojs.mergeOptions(dummyParams, {
+        'a': action,
+        'pageid': player.neon.pageId,
+        'cts': (new Date()).getTime()
+    });
+    let url = player.neon.options['trackUrl'];
+    console.log('remote', action, url, data)
 
     /*
      Resolve the CORS problem; implement the rest of tracked data
@@ -115,13 +152,18 @@ const remoteLogEvent = (eventType, extra) => {
         method: 'get',
         crossDomain: true,
         data: data,
-        success: function(response) {
-            console.log(response);
-        }
+        success: function(response) { }
     });
     /**/
 };
 
+// Taken from the other js client code; need to know if this is fine
+const _uuid = () => {
+    function genRandomHexChars() {
+        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    }
+    return genRandomHexChars() + genRandomHexChars() + genRandomHexChars() + genRandomHexChars();
+}
 
 // Register the plugin with video.js.
 videojs.plugin('neon', neonTracker);
