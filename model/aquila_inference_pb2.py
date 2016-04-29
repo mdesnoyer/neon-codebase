1bfa74b7da0a330138397cb18e737e0455c8d199
Nick Dufour <ndufour@Davids-MacBook-Air.local>
2016-04-29 10:13:17 -0700

    apparently protobuf is always installed in the google module

--- model/aquila_inference_pb2.py
+++ model/aquila_inference_pb2.py
@@ -3,11 +3,11 @@
 
 import sys
 _b=sys.version_info[0]<3 and (lambda x:x) or (lambda x:x.encode('latin1'))
-from protobuf import descriptor as _descriptor
-from protobuf import message as _message
-from protobuf import reflection as _reflection
-from protobuf import symbol_database as _symbol_database
-from protobuf import descriptor_pb2
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from google.protobuf import reflection as _reflection
+from google.protobuf import symbol_database as _symbol_database
+from google.protobuf import descriptor_pb2
 # @@protoc_insertion_point(imports)
 
 _sym_db = _symbol_database.Default()
