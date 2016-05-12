550ff460b1ab38f8765aecb02e4b4dca49246ce9
Nate Kresge <kresge@neon-lab.com>
2016-05-12 16:07:16 -0700

    Require account id (even if not used)

--- cmsapiv2/controllers.py
+++ cmsapiv2/controllers.py
@@ -451,9 +451,11 @@ class BrightcovePlayerHandler(APIV2Handler):
 
         # Validate request and data
         schema = Schema({
-            Required('integration_id'): Any(str, unicode, Length(min=1, max=256))
+            Required('account_id'): All(Coerce(str), Length(min=1, max=256)),
+            Required('integration_id'): All(Coerce(str), Length(min=1, max=256))
         })
         args = self.parse_args()
+        args['account_id'] = account_id = str(account_id)
         schema(args)
         integration_id = args['integration_id']
         integration = yield neondata.BrightcoveIntegration.get(integration_id, async=True)
@@ -504,8 +506,8 @@ class BrightcovePlayerHandler(APIV2Handler):
         # The only field that is set via public api is is_tracked.
         # @TODO fix the Any() below
         schema = Schema({
-            Required('account_id'): Any(str, unicode, Length(min=1, max=256)),
-            Required('player_ref'): Any(str, unicode, Length(min=1, max=256)),
+            Required('account_id'): All(Coerce(str), Length(min=1, max=256)),
+            Required('player_ref'): All(Coerce(str), Length(min=1, max=256)),
             Required('is_tracked'): Boolean()
         })
         args = self.parse_args()
