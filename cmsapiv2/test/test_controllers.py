44784812a6df72b4e2b6968a4980b27af99d135f
Kevin Fenger <fenger@neon-lab.com>
2016-05-17 14:20:53 -0700

    add default experiment strategy to every account

--- cmsapiv2/test/test_controllers.py
+++ cmsapiv2/test/test_controllers.py
@@ -254,6 +254,10 @@ class TestNewAccountHandler(TestAuthenticationBase):
         limits = yield neondata.AccountLimits.get(account_id, async=True)
         self.assertEquals(limits.key, account_id) 
         self.assertEquals(limits.video_posts, 0)
+
+        exps = yield neondata.ExperimentStrategy.get(account_id, async=True)
+        self.assertEquals(exps.get_id(), account_id) 
+        self.assertEquals(exps.exp_frac, 0.01)
         
     @tornado.testing.gen_test 
     def test_create_new_account_json(self):
@@ -301,6 +305,10 @@ class TestNewAccountHandler(TestAuthenticationBase):
         limits = yield neondata.AccountLimits.get(account_id, async=True)
         self.assertEquals(limits.key, account_id) 
         self.assertEquals(limits.video_posts, 0)
+
+        exps = yield neondata.ExperimentStrategy.get(account_id, async=True)
+        self.assertEquals(exps.get_id(), account_id) 
+        self.assertEquals(exps.exp_frac, 0.01)
 
     @tornado.testing.gen_test 
     def test_create_new_account_uppercase_username(self):
