19f183d955b0b5d30991dad518ea5fe3e6c9b8b6
nadeem86 <nadeem.ahmed86@gmail.com>
2016-05-18 10:34:43 -0700

    re-wiring batch process to output to hdfs instead of s3

--- stats/cluster_manager.py
+++ stats/cluster_manager.py
@@ -108,7 +108,7 @@ class BatchProcessManager(threading.Thread):
                 self.cluster.change_instance_group_size(
                     'TASK', new_size=self.n_task_instances)
                 stats.batch_processor.run_batch_cleaning_job(
-                    self.cluster, 's3://neon-tracker-logs-v2/v2.2/1930337906/2016/05/17/*',
+                    self.cluster, 's3://neon-tracker-logs-v2/v2.2/1930337906/2016/*/*/*',
                     cleaned_output_path,
                     timeout = (options.batch_period * 10))
                 _log.info('Sucessful cleaning job output to: %s' %
