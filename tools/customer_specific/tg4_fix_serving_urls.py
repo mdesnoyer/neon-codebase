aee627c4e548431b52c779a75c6c79ea0bba95b9
mdesnoyer <mdesnoyer@gmail.com>
2016-06-01 16:45:11 -0700

    Handle missing thumbnail src in Brightcove Response (#552)
    * Push same thumbnail size into Brightcove
    
    * Better re
    
    * Fix TG4 script
    
    * Fix TG4 bug

--- tools/customer_specific/tg4_fix_serving_urls.py
+++ tools/customer_specific/tg4_fix_serving_urls.py
@@ -15,16 +15,31 @@ __base_path__ = os.path.abspath(os.path.join(os.path.dirname(__file__), '..',
 if sys.path[0] != __base_path__:
     sys.path.insert(0, __base_path__)
 
-import api.brightcove
+import api.brightcove_api
 from cmsdb import neondata
+import logging
 import tornado.gen
 import tornado.ioloop
+import utils.neon
 from utils.options import define, options
+
+_log = logging.getLogger(__name__)
 
 @tornado.gen.coroutine
 def process_one_video(bcapi, video_id):
     _log.info('Processing video: %s' % video_id)
-    video = yield neondata.VideoMetadata.get(video_id
+    video = yield neondata.VideoMetadata.get(video_id, async=True)
+
+    new_serving_url = yield video.get_serving_url(async=True)
+    new_serving_url += '?height=262&width=480'
+
+    bc_vid = neondata.InternalVideoID.to_external(video_id)
+    video_data = yield bcapi.get_video_images(bc_vid)
+
+    poster = video_data['poster']
+    if 'neon-images.com' in poster['src']:
+        yield bcapi.update_poster(bc_vid, poster['asset_id'],
+                                  new_serving_url)
 
 @tornado.gen.coroutine
 def main():
@@ -35,11 +50,15 @@ def main():
     integ = yield neondata.BrightcoveIntegration.get(
         'fef5ef35a5dd473f8f80b735f5fa49f4', async=True)
 
-    bcapi = api.brightcove.CMSAPI(integ.publisher_id,
-                                  integ.application_client_id,
-                                  integ.application_client_secret)
+    bcapi = api.brightcove_api.CMSAPI(integ.publisher_id,
+                                      integ.application_client_id,
+                                      integ.application_client_secret)
 
-    yield [process_one_video(bcapi, x) for x in video_ids]
+    for vid in video_ids:
+        try:
+            yield process_one_video(bcapi, vid)
+        except Exception as e:
+            _log.error('Error processing video %s: %s' % (vid, e))
 
 if __name__ == "__main__":
     utils.neon.InitNeon()
