8b6141a1d3556e5afcc598a6e4e132cfe5570e3b
nadeem86 <nadeem.ahmed86@gmail.com>
2016-06-22 11:35:32 -0700

    fix input path

--- stats/airflow/dags/neon.py
+++ stats/airflow/dags/neon.py
@@ -491,8 +491,10 @@ def _stage_files(**kwargs):
     _log.info('staging bucket is %s' % staging_bucket)
     _log.info('staging prefix is %s' % staging_prefix)
 
-    input_files = _get_s3_input_files(dag=dag, execution_date=execution_date,
-                                      task=task, input_path=input_path)
+    input_files = _get_s3_input_files(dag=dag,
+                                      execution_date=execution_date,
+                                      task=task,
+                                      input_path=kwargs['input_path'])
 
     output_prefix = _get_s3_staging_prefix(dag=dag,
                                            execution_date=execution_date,
