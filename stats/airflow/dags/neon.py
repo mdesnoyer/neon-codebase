8f0e692f4455088078a038ba374e12cecc71fc70
nadeem86 <nadeem.ahmed86@gmail.com>
2016-07-01 00:36:53 -0700

    fix execution date

--- stats/airflow/dags/neon.py
+++ stats/airflow/dags/neon.py
@@ -602,7 +602,8 @@ def _load_impala_table(**kwargs):
         builder = stats.impala_table.ImpalaTableLoader(
             cluster=cluster,
             event=event,
-            execution_date=executation_date,
+            #execution_date=executation_date,
+            execution_date=execution_date,
             hour_interval=dag.schedule_interval.total_seconds()/3600,
             input_path=os.path.join('s3://', output_bucket, cleaned_prefix))
 
