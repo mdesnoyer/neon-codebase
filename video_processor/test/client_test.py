bf0714a54a71f494c9b537ff2d5dd75cf49222ad
Nick <dufour@neon-lab.com>
2016-07-14 17:05:54 -0700

    .

--- video_processor/test/client_test.py
+++ video_processor/test/client_test.py
@@ -1100,7 +1100,7 @@ class TestFinalizeResponse(test_utils.neontest.AsyncTestCase):
         self.assertEquals(tas['thumbnail_one'], 'second_best')  
         self.assertEquals(tas['thumbnail_two'], 'third_best')  
         self.assertEquals(tas['thumbnail_three'], 'fourth_best')  
-        self.assertEquals(tas['lift'], 2.167) 
+        self.assertEquals(tas['lift'], 0.667) 
         self.assertEquals(rv, True)
  
     @tornado.testing.gen_test
@@ -1908,7 +1908,7 @@ class TestFinalizeResponse(test_utils.neontest.AsyncTestCase):
         self.assertEquals(tas['thumbnail_one'], 'second_best')  
         self.assertEquals(tas['thumbnail_two'], 'third_best')  
         self.assertEquals(tas['thumbnail_three'], 'fourth_best')  
-        self.assertEquals(tas['lift'], 2.167) 
+        self.assertEquals(tas['lift'], 0.667) 
  
     @tornado.testing.gen_test
     def test_get_template_args_no_tn_exception(self):
