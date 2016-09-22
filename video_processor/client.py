b2e96ee8a75770fe8f2559d7669896e641abe0a2
Nate Kresge <kresge@neon-lab.com>
2016-09-22 16:07:53 -0700

    Incremental

--- video_processor/client.py
+++ video_processor/client.py
@@ -62,6 +62,10 @@ from video_processor import video_processing_queue
 import logging
 _log = logging.getLogger(__name__)
 
+# Hide warnings from image process.
+logging.getLogger('sklearn').setLevel(logging.ERROR)
+logging.getLogger('scipy').setLevel(logging.ERROR)
+
 #Monitoring
 statemon.define('processed_video', int)
 statemon.define('processing_error', int)
