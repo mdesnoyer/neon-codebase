6f9649396dc3a4898f76e62634ef109b28edb66a
Mark Desnoyer <desnoyer@neon-lab.com>
2016-10-19 17:13:18 -0700

    Add script to get memnet scores

--- tools/customer_specific/facebook_memnet_video_list_score.py
+++ tools/customer_specific/facebook_memnet_video_list_score.py
@@ -17,7 +17,8 @@ Copyright 2016 Neon Labs Inc.
 import os
 import os.path
 import sys
-__base_path__ = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+__base_path__ = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 
+                                             '..'))
 if sys.path[0] != __base_path__:
     sys.path.insert(0, __base_path__)
 
