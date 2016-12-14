28d93a23f18afcacc57208d1d152f316f53d5660
Nate Kresge <kresge@neon-lab.com>
2016-12-14 09:22:00 -0800

    Consistent quote

--- utils/neon.py
+++ utils/neon.py
@@ -80,14 +80,14 @@ def EnableRunningDebugging():
 
 @contextlib.contextmanager
 def set_env(**environ):
-    """
+    '''
     Temporarily set the process environment variables.
 
     >>> with set_env(PLUGINS_DIR=u'test/plugins'):
 
     :type environ: dict[str, unicode]
     :param environ: Environment variables to set
-    """
+    '''
     old_environ = dict(os.environ)
     os.environ.update(environ)
     try:
